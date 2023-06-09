351b2d0787658227f07fd5346f3360fb3c3da48a
Aleksander Kisielewicz <aleksander.kisielewicz@gmail.com>
2023-06-09 18:18:21 +0000

    Add Switch and NavBar component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
-import './App.css';
+import styles from './App.module.css';
+import NavBar from "./components/NavBar";
+import { Route, Switch } from "react-router-dom";
+// import SignUpForm from "./pages/auth/SignUpForm";
+// import SignInForm from "./pages/auth/SignInForm";
 
 function App() {
   return (
-    <div className="App">
-      <h1>It's working</h1>
+    <div className={styles.App}>
+      <Switch>
+        <Route exact path="/" render={() => <h1>Home</h1>} />
+        <Route exact path="/signin" render={() => <h1>Sign Up</h1>} />
+        <Route exact path="/signup" render={() => <h1>Sign Up</h1>} />
+        <Route render={() => <h1>Page not found!</h1>}/>
+      </Switch>
+      <NavBar />
     </div>
   );
 }
