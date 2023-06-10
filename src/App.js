13dc51d491efeea24155eddb06e7d84e2fe51952
Aleksander Kisielewicz <aleksander.kisielewicz@gmail.com>
2023-06-10 09:38:41 +0000

    Change routes to nav pages

--- src/App.js
+++ src/App.js
@@ -1,19 +1,24 @@
-import styles from './App.module.css';
+import styles from "./App.module.css";
 import NavBar from "./components/NavBar";
+import Container from "react-bootstrap/Container";
 import { Route, Switch } from "react-router-dom";
-// import SignUpForm from "./pages/auth/SignUpForm";
-// import SignInForm from "./pages/auth/SignInForm";
+import "./api/axiosDefaults";
+import SignUpForm from "./pages/auth/SignUpForm";
+import SignInForm from "./pages/auth/SignInForm";
+
 
 function App() {
   return (
     <div className={styles.App}>
-      <Switch>
-        <Route exact path="/" render={() => <h1>Home</h1>} />
-        <Route exact path="/signin" render={() => <h1>Sign Up</h1>} />
-        <Route exact path="/signup" render={() => <h1>Sign Up</h1>} />
-        <Route render={() => <h1>Page not found!</h1>}/>
-      </Switch>
       <NavBar />
+      <Container className={styles.Wrapper}>
+        <Switch>
+          <Route exact path="/" render={() => <h1>Home</h1>} />
+          <Route exact path="/signin" render={() => <SignInForm />} />
+          <Route exact path="/signup" render={() => <SignUpForm />} />
+          <Route render={() => <h1>Page not found!</h1>}/>
+        </Switch>
+      </Container>
     </div>
   );
 }
