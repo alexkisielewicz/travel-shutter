cd0e2999de8f5af8557c4d27090e8182efb0a18d
Aleksander Kisielewicz <aleksander.kisielewicz@gmail.com>
2023-06-11 12:57:33 +0000

    Add route to post detail view

--- src/App.js
+++ src/App.js
@@ -21,7 +21,7 @@ function App() {
               <Route exact path="/signin" render={() => <SignInForm />} />
               <Route exact path="/signup" render={() => <SignUpForm />} />
               <Route exact path="/posts/create" render={() => <PostCreateForm />} /> 
-              <Route exact path="/post/:id" render={() => <PostPage />} />
+              <Route exact path="/posts/:id" render={() => <PostPage />} />
               <Route render={() => <Page404 />} />
             </Switch>
           </Container>
