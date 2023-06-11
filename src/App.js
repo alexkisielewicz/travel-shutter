4921f65c6084377a4141b3d5b0ba9d0991f4ad04
Aleksander Kisielewicz <aleksander.kisielewicz@gmail.com>
2023-06-11 16:36:41 +0000

    Correct route for home and feed pages

--- src/App.js
+++ src/App.js
@@ -15,40 +15,45 @@ import { useCurrentUser } from "./contexts/CurrentUserContext";
 function App() {
   const currentUser = useCurrentUser();
   const profile_id = currentUser?.profile_id || "";
-  
+
   return (
-        <div className={styles.App}>
-          <NavBar />
-          <Container className={styles.Wrapper}>
-            <Switch>
-              <Route 
-                exact 
-                path="/" 
-                render={() => <PostsPage />} 
-                message="No results found. Adjust the search keyword." 
+    <div className={styles.App}>
+      <NavBar />
+      <Container className={styles.Wrapper}>
+        <Switch>
+          <Route
+            exact
+            path="/"
+            render={() => <PostsPage />}
+            message="No results found. Adjust the search keyword."
+          />
+          <Route
+            exact
+            path="/feed"
+            render={() => (
+              <PostsPage
+                message="No results found. Adjust search keyword or follow a user."
+                filter={`owner__followed__owner__profile=${profile_id}&`} />
+            )}
+          />
+          <Route
+            exact
+            path="/liked"
+            render={() => (
+              <PostsPage
+                message="No results found. Adjust the search keyword or like a post."
+                filter={`likes__owner__profile=${profile_id}&ordering=-likes__created_at&`}
               />
-              <Route 
-                exact 
-                path="/feed" 
-                render={() => <PostsPage />} 
-                message="No results found. Adjust search keyword or follow a user." 
-                filter={`owner__followed__owner__profile=${profile_id}&`} 
-              />
-              <Route 
-                exact 
-                path="/liked" 
-                render={() => <PostsPage />} 
-                message="No results found. Adjust the search keyword or like apost." 
-                filter={`likes__owner__profile=${profile_id}&ordering=-likes__created_at&`} 
-              />
-              <Route exact path="/signin" render={() => <SignInForm />} />
-              <Route exact path="/signup" render={() => <SignUpForm />} />
-              <Route exact path="/posts/create" render={() => <PostCreateForm />} /> 
-              <Route exact path="/posts/:id" render={() => <PostPage />} />
-              <Route render={() => <Page404 />} />
-            </Switch>
-          </Container>
-        </div>
+            )}
+          />
+          <Route exact path="/signin" render={() => <SignInForm />} />
+          <Route exact path="/signup" render={() => <SignUpForm />} />
+          <Route exact path="/posts/create" render={() => <PostCreateForm />} />
+          <Route exact path="/posts/:id" render={() => <PostPage />} />
+          <Route render={() => <Page404 />} />
+        </Switch>
+      </Container>
+    </div>
   );
 }
 
