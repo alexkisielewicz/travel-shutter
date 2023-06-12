40d271615ef313987e8178e8bcd9f06a7f7c05b0
Aleksander Kisielewicz <aleksander.kisielewicz@gmail.com>
2023-06-12 16:38:55 +0000

    Add list of comments with infinite scroll

--- src/pages/posts/PostPage.js
+++ src/pages/posts/PostPage.js
@@ -5,13 +5,16 @@ import Row from "react-bootstrap/Row";
 import Container from "react-bootstrap/Container";
 
 import appStyles from "../../App.module.css";
-import { useParams } from "react-router-dom/cjs/react-router-dom.min";
+import { useParams } from "react-router";
 import { axiosReq } from "../../api/axiosDefaults";
-import SinglePost from "./SinglePost";
+import PostContainer from "./PostContainer";
+import Comment from "../comments/Comment";
 
 import CommentCreateForm from "../comments/CommentCreateForm";
 import { useCurrentUser } from "../../contexts/CurrentUserContext";
-import Comment from "../comments/Comment";
+import InfiniteScroll from "react-infinite-scroll-component";
+import Asset from "../../components/Asset";
+import { fetchMoreData } from "../../utils/utils";
 
 function PostPage() {
   const { id } = useParams();
@@ -24,28 +27,25 @@ function PostPage() {
   useEffect(() => {
     const handleMount = async () => {
       try {
-        const [{ data: post }, { data: comments}] = await Promise.all([
+        const [{ data: post }, { data: comments }] = await Promise.all([
           axiosReq.get(`/posts/${id}`),
-          axiosReq.get(`/comments/?post=${id}`)
-        ])
+          axiosReq.get(`/comments/?post=${id}`),
+        ]);
         setPost({ results: [post] });
         setComments(comments);
-      } catch (error) {
-        console.log(error)
+      } catch (err) {
+        console.log(err);
       }
-    }
+    };
 
-    handleMount()
-    // call handleMount() every time that post id change
-  }, [id])
+    handleMount();
+  }, [id]);
 
   return (
     <Row className="h-100">
       <Col className="py-2 p-0 p-lg-2" lg={8}>
         <p>Popular profiles for mobile</p>
-
-        <SinglePost {...post.results[0]} setPosts={setPost} postPage />
-
+        <PostContainer {...post.results[0]} setPosts={setPost} postPage />
         <Container className={appStyles.Content}>
           {currentUser ? (
             <CommentCreateForm
@@ -58,14 +58,28 @@ function PostPage() {
           ) : comments.results.length ? (
             "Comments"
           ) : null}
+          {/* ALL COMMENTS WITH INFINITE SCROLL */}
           {comments.results.length ? (
-            comments.results.map(comment => (
-              <Comment key={comment.id} {...comment}/>
-            ))
+            <InfiniteScroll
+              children={
+                comments.results.map((comment) => (
+                  <Comment
+                    key={comment.id}
+                    {...comment}
+                    setPost={setPost}
+                    setComments={setComments}
+                  />
+                ))
+              }
+              dataLength={comments.results.length}
+              loader={<Asset spinner />}
+              hasMore={!!comments.next}
+              next={() => fetchMoreData(comments, setComments)}
+            />
           ) : currentUser ? (
-            <span>No comments yet, be the first one to comment!</span>
+            <span>No comments yet, be the first to comment!</span>
           ) : (
-            <span>No comments yet.</span>
+            <span>No comments... yet</span>
           )}
         </Container>
       </Col>
