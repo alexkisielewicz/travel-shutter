3f4c0cdfdddf6261b4e344895a80957c36038433
Aleksander Kisielewicz <aleksander.kisielewicz@gmail.com>
2023-06-12 21:03:57 +0000

    Add route to profile

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ import PostPage from "./pages/posts/PostPage";
 import PostsPage from "./pages/posts/PostsPage";
 import { useCurrentUser } from "./contexts/CurrentUserContext";
 import PostEditForm from "./pages/posts/PostEditForm";
+import ProfilePage from "./pages/profiles/ProfilePage";
 
 
 function App() {
@@ -52,6 +53,7 @@ function App() {
           <Route exact path="/posts/create" render={() => <PostCreateForm />} />
           <Route exact path="/posts/:id/edit" render={() => <PostEditForm />} />
           <Route exact path="/posts/:id" render={() => <PostPage />} />
+          <Route exact path="/profiles/:id" render={() => <ProfilePage />} />
           <Route render={() => <Page404 />} />
         </Switch>
       </Container>
