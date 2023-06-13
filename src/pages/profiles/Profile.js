a9b14041ec921b0a4bd868330699cd290268313e
Aleksander Kisielewicz <aleksander.kisielewicz@gmail.com>
2023-06-13 13:23:54 +0000

    Add header with user name and picture to profile

--- src/pages/profiles/Profile.js
+++ src/pages/profiles/Profile.js
@@ -2,55 +2,54 @@ import React from "react";
 import styles from "../../styles/Profile.module.css";
 import btnStyles from "../../styles/Button.module.css";
 import { useCurrentUser } from "../../contexts/CurrentUserContext";
-import { Link } from "react-router-dom/cjs/react-router-dom.min";
+import { Link } from "react-router-dom";
 import Avatar from "../../components/Avatar";
 import { Button } from "react-bootstrap";
-
+import { useSetProfileData } from "../../contexts/ProfileDataContext";
 
 const Profile = (props) => {
-  const {
-    mobile,
-    profile,
-    imageSize = 55,
-  } = props;
-
+  const { profile, mobile, imageSize = 55 } = props;
   const { id, following_id, image, owner } = profile;
 
   const currentUser = useCurrentUser();
-  const is_owner = currentUser?.username === owner; // bool
+  const is_owner = currentUser?.username === owner;
+
+  const { handleFollow, handleUnfollow } = useSetProfileData();
 
   return (
-    <div className={`my-3 d-flex align-items-center ${mobile && "flex-column"}`}>
-      <Link
-        to={`/profile/${id}`}
-        className="align-self-center"
-      >
-        <Avatar src={image} height={imageSize} />
-      </Link>
+    <div
+      className={`my-3 d-flex align-items-center ${mobile && "flex-column"}`}
+    >
+      <div>
+        <Link className="align-self-center" to={`/profiles/${id}`}>
+          <Avatar src={image} height={imageSize} />
+        </Link>
+      </div>
       <div className={`mx-2 ${styles.WordBreak}`}>
         <strong>{owner}</strong>
       </div>
       <div className={`text-right ${!mobile && "ml-auto"}`}>
-        {!mobile && currentUser && !is_owner && (
-          following_id ? (
+        {!mobile &&
+          currentUser &&
+          !is_owner &&
+          (following_id ? (
             <Button
               className={`${btnStyles.Button} ${btnStyles.BlackOutline}`}
-              onClick={() => { }}
+              onClick={() => handleUnfollow(profile)}
             >
               unfollow
             </Button>
           ) : (
             <Button
-              className={`${btnStyles.Button} ${btnStyles.Blac}`}
-              onClick={() => {}}
+              className={`${btnStyles.Button} ${btnStyles.Black}`}
+              onClick={() => handleFollow(profile)}
             >
               follow
             </Button>
-          )
-        )}
+          ))}
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Profile
+export default Profile;
