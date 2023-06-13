26268f01fcca5669ba4e9f18cfbd9516b3815aa4
Aleksander Kisielewicz <aleksander.kisielewicz@gmail.com>
2023-06-13 16:09:57 +0000

    Create ProfileEditForm

---
+++ src/pages/profiles/ProfileEditForm.js
@@ -0,0 +1,204 @@
+import React, { useState, useEffect, useRef } from "react";
+import { useHistory, useParams } from "react-router-dom";
+
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
+import Image from "react-bootstrap/Image";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Container from "react-bootstrap/Container";
+import Alert from "react-bootstrap/Alert";
+
+import { axiosReq } from "../../api/axiosDefaults";
+import {
+  useCurrentUser,
+  useSetCurrentUser,
+} from "../../contexts/CurrentUserContext";
+
+import btnStyles from "../../styles/Button.module.css";
+import styles from "../../styles/ProfileEditForm.module.css";
+import appStyles from "../../App.module.css";
+
+const ProfileEditForm = () => {
+  const currentUser = useCurrentUser();
+  const setCurrentUser = useSetCurrentUser();
+  const { id } = useParams();
+  const history = useHistory();
+  const imageFile = useRef();
+
+  const [profileData, setProfileData] = useState({
+    name: "",
+    bio: "",
+    instagram: "",
+    equipment: "",
+    image: "",
+  });
+  const { name, bio, instagram, equipment, image } = profileData;
+
+  const [errors, setErrors] = useState({});
+
+  useEffect(() => {
+    const handleMount = async () => {
+      if (currentUser?.profile_id?.toString() === id) {
+        try {
+          const { data } = await axiosReq.get(`/profiles/${id}`);
+          const { name, bio, instagram, equipment, image } = data;
+          setProfileData({ name, bio, instagram, equipment, image });
+        } catch (error) {
+          console.log(error);
+          history.push("/");
+        }
+      } else {
+        history.push("/");
+      }
+    };
+
+    handleMount();
+  }, [currentUser, history, id]);
+
+  const handleChange = (event) => {
+    setProfileData({
+      ...profileData,
+      [event.target.name]: event.target.value,
+    });
+  };
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    const formData = new FormData();
+    formData.append("name", name);
+    formData.append("bio", bio);
+    formData.append("instagram", instagram);
+    formData.append("equipment", equipment);
+
+    if (imageFile?.current?.files[0]) {
+      formData.append("image", imageFile?.current?.files[0]);
+    }
+
+    try {
+      const { data } = await axiosReq.put(`/profiles/${id}`, formData);
+      setCurrentUser((currentUser) => ({
+        ...currentUser,
+        profile_image: data.image,
+      }));
+      history.goBack();
+    } catch (err) {
+      console.log(err);
+      setErrors(err.response?.data);
+    }
+  };
+
+  const textFields = (
+    <>
+      <Form.Group>
+        <Form.Label>Name</Form.Label>
+        <Form.Control
+          as="textarea"
+          value={name}
+          onChange={handleChange}
+          name="name"
+          rows={1}
+        />
+      </Form.Group>
+
+      <Form.Group>
+        <Form.Label>Bio</Form.Label>
+        <Form.Control
+          as="textarea"
+          value={bio}
+          onChange={handleChange}
+          name="bio"
+          rows={3}
+        />
+      </Form.Group>
+
+      <Form.Group>
+        <Form.Label>Instagram @</Form.Label>
+        <Form.Control
+          as="textarea"
+          value={instagram}
+          onChange={handleChange}
+          name="instagram"
+          rows={5}
+        />
+      </Form.Group>
+
+      <Form.Group>
+        <Form.Label>Equipment</Form.Label>
+        <Form.Control
+          as="textarea"
+          value={equipment}
+          onChange={handleChange}
+          name="equipment"
+          rows={4}
+        />
+      </Form.Group>
+
+      {errors?.content?.map((message, idx) => (
+        <Alert variant="warning" key={idx}>
+          {message}
+        </Alert>
+      ))}
+      <Button
+        className={`${btnStyles.Button} ${btnStyles.Blue}`}
+        onClick={() => history.goBack()}
+      >
+        cancel
+      </Button>
+      <Button className={`${btnStyles.Button} ${btnStyles.Blue}`} type="submit">
+        save
+      </Button>
+    </>
+  );
+
+  return (
+    <Form onSubmit={handleSubmit}>
+      <Row>
+        <Col className="py-2 p-0 p-md-2 text-center" md={7} lg={6}>
+          <Container className={styles.Container}>
+            <Form.Group>
+              {image && (
+                <figure>
+                  <Image src={image} fluid />
+                </figure>
+              )}
+              {errors?.image?.map((message, idx) => (
+                <Alert variant="warning" key={idx}>
+                  {message}
+                </Alert>
+              ))}
+              <div>
+                <Form.Label
+                  className={`${btnStyles.Button} ${btnStyles.Blue} btn my-auto`}
+                  htmlFor="image-upload"
+                >
+                  Change the image
+                </Form.Label>
+              </div>
+              <Form.File
+                className={styles.FileInput}
+                id="image-upload"
+                ref={imageFile}
+                accept="image/*"
+                onChange={(e) => {
+                  if (e.target.files.length) {
+                    setProfileData({
+                      ...profileData,
+                      image: URL.createObjectURL(e.target.files[0]),
+                    });
+                  }
+                }}
+              />
+            </Form.Group>
+            <div className="d-md-none">{textFields}</div>
+          </Container>
+        </Col>
+        <Col md={5} lg={6} className="d-none d-md-block p-0 p-md-2 text-center">
+          <Container className={styles.TextFields}>{textFields}</Container>
+        </Col>
+      </Row>
+    </Form>
+  );
+};
+
+export default ProfileEditForm;
