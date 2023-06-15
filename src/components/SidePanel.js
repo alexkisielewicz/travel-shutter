f498fad156aa1c79345f9b991a39e0d72622e926
Aleksander Kisielewicz <aleksander.kisielewicz@gmail.com>
2023-06-15 18:58:07 +0000

    Create SidePanel component

--- src/components/SidePanel.js
+++ src/components/SidePanel.js
@@ -1,27 +1,67 @@
 import React from "react";
 import appStyles from "../App.module.css";
+import styles from "../styles/SidePanel.module.css";
 import { Container } from "react-bootstrap";
+import { useCurrentUser } from "../contexts/CurrentUserContext";
+import { NavLink } from "react-router-dom/cjs/react-router-dom.min";
 
 
 const SidePanel = ({ mobile }) => {
 
+  const currentUser = useCurrentUser();
+
+  const addPostIcon = (
+    <NavLink
+      className={styles.NavLink}
+      activeClassName={styles.Active}
+      to="/posts/create"
+    >
+      <i className="far fa-plus-square"></i>Add post
+    </NavLink>
+  )
+
+  const loggedInIcons = (
+    <>
+      <NavLink
+        className={styles.NavLink}
+        activeClassName={styles.Active}
+        to="/feed"
+      >
+        <i className="fas fa-stream"></i>Feed
+      </NavLink>
+
+      <NavLink
+        className={styles.NavLink}
+        activeClassName={styles.Active}
+        to="/liked"
+      >
+        <i className="fas fa-heart"></i>Liked
+      </NavLink>
+    </>
+  );
+
 
   return (
     <Container className={`${appStyles.Container} ${mobile && 'd-lg-none text-center mb-3'}`}>
-        <>
-          <h5 className="text-center">Menu</h5>
-          {mobile ? (
-            // IF ON MOBILE
-            <div className="d-flex justify-content-around">
-              Side panel test MOBILE
-            </div>
-          ) : (
-            // IF ON DESKTOP
-            <p>Side panel test DESKTOP</p>
-          )}
-        </>
+      <>
+        <h5 className="text-center pb-3">{mobile ? (<></>) : (`Menu`) }</h5>
+        {mobile ? (
+          // IF ON MOBILE
+          <div className="d-flex justify-content-around">
+            {currentUser && addPostIcon}
+            {currentUser ? loggedInIcons : <></>}
+          </div>
+        ) : (
+          // IF ON DESKTOP
+          <>
+            {currentUser && addPostIcon}
+            {currentUser ? loggedInIcons : <></>}
+          </>
+
+        )}
+      </>
     </Container>
-   
+
   );
 };
 
