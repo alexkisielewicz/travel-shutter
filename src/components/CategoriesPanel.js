9b1b80509d1397ac108444dc107341c21ef19a90
Aleksander Kisielewicz <aleksander.kisielewicz@gmail.com>
2023-06-16 15:33:50 +0000

    Add custom spinner to component

--- src/components/CategoriesPanel.js
+++ src/components/CategoriesPanel.js
@@ -4,6 +4,7 @@ import btnStyles from "../styles/Button.module.css";
 import { Button, Container } from "react-bootstrap";
 import axios from 'axios';
 import { Link } from "react-router-dom/cjs/react-router-dom.min";
+import Spinner from "../components/Spinner";
 
 const CategoriesPanel = ({ mobile }) => {
   const [categories, setCategories] = useState([]);
@@ -12,10 +13,9 @@ const CategoriesPanel = ({ mobile }) => {
     const fetchCategories = async () => {
       try {
         const response = await axios.get('/categories/');
-        console.log(response);
         setCategories(response.data['Category list']);
       } catch (error) {
-        console.error('Error fetching categories:', error);
+        console.log(error);
       }
     };
     fetchCategories();
@@ -23,34 +23,38 @@ const CategoriesPanel = ({ mobile }) => {
 
   return (
     <Container className={`${appStyles.Container} ${mobile && 'd-lg-none text-center mb-3'}`}>
-      <>
-        {mobile ? (
-          // IF ON MOBILE
-          <div className="d-flex justify-content-around">
-            Categories panel MOBILE
-          </div>
-        ) : (
-          // IF ON DESKTOP
-          
-            // <h5 className="text-center pb-3">Categories</h5>
+      {categories.length ? (
+        <>
+          {mobile ? (
+            // IF ON MOBILE
+            <div className="d-flex justify-content-around">
+              Categories panel MOBILE
+            </div>
+          ) : (
+            // IF ON DESKTOP
             <div className="d-flex py-0">
-            <p className="text-center py-0 m-0">
-              {categories.map((category, index) => (
-                <Button
-                  key={index}
-                  className={`${btnStyles.Button} ${btnStyles.Category}`}
-                  onClick={() => {}}
-                >
-                  <Link>{category}</Link>
-                </Button>
-              ))}
-            </p>
-          </div>
-          
-        )}
-      </>
+              <p className="text-center py-0 m-0">
+                {categories.map((category, index) => (
+                  <Button
+                    key={index}
+                    className={`${btnStyles.Button} ${btnStyles.Category}`}
+                    onClick={() => {}}
+                  >
+                    <Link to={`/posts/?category=${category}`}>{category}</Link>
+                  </Button>
+                ))}
+              </p>
+            </div>
+          )}
+        </>
+      ) : (
+        <div className="text-center">
+          <Spinner />
+        </div>
+      )}
     </Container>
   );
+  
 };
 
 export default CategoriesPanel;
