5fe685a41bd490d01c4b83191d146b0c6c2fccec
Aleksander Kisielewicz <aleksander.kisielewicz@gmail.com>
2023-06-16 11:45:30 +0000

    Add buttons with categories

--- src/components/CategoriesPanel.js
+++ src/components/CategoriesPanel.js
@@ -1,12 +1,12 @@
 import React, { useState, useEffect } from "react";
 import appStyles from "../App.module.css";
-import { Container } from "react-bootstrap";
+import btnStyles from "../styles/Button.module.css";
+import { Button, Container } from "react-bootstrap";
 import axios from 'axios';
 import { Link } from "react-router-dom/cjs/react-router-dom.min";
 
 const CategoriesPanel = ({ mobile }) => {
   const [categories, setCategories] = useState([]);
-  
 
   useEffect(() => {
     const fetchCategories = async () => {
@@ -24,7 +24,6 @@ const CategoriesPanel = ({ mobile }) => {
   return (
     <Container className={`${appStyles.Container} ${mobile && 'd-lg-none text-center mb-3'}`}>
       <>
-        <h5 className="text-center">Categories</h5>
         {mobile ? (
           // IF ON MOBILE
           <div className="d-flex justify-content-around">
@@ -32,13 +31,22 @@ const CategoriesPanel = ({ mobile }) => {
           </div>
         ) : (
           // IF ON DESKTOP
-          <>
-            <ul>
+          
+            // <h5 className="text-center pb-3">Categories</h5>
+            <div className="d-flex py-0">
+            <p className="text-center py-0 m-0">
               {categories.map((category, index) => (
-                <li key={index}><Link to={`/posts/?category=${category}`}>{category}</Link></li>
+                <Button
+                  key={index}
+                  className={`${btnStyles.Button} ${btnStyles.Category}`}
+                  onClick={() => {}}
+                >
+                  <Link>{category}</Link>
+                </Button>
               ))}
-            </ul>
-          </>
+            </p>
+          </div>
+          
         )}
       </>
     </Container>
