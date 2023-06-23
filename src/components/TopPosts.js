61b5dc296dd8dcd77629c8fd0c43a9e966d3a8f2
Aleksander Kisielewicz <aleksander.kisielewicz@gmail.com>
2023-06-23 13:07:51 +0000

    Modify axios request to handle api pagination

--- src/components/TopPosts.js
+++ src/components/TopPosts.js
@@ -3,56 +3,79 @@ import appStyles from "../App.module.css";
 import styles from "../styles/TopPosts.module.css";
 
 import axios from 'axios';
-
 import { Link } from 'react-router-dom/cjs/react-router-dom.min';
-import { Col, Container, Row } from 'react-bootstrap';
 
-function TopPosts() {
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Spinner from './Spinner';
+
+function TopPosts({ refreshLikes }) {
   const [posts, setPosts] = useState([]);
+  const [hasLoaded, setHasLoaded] = useState(false);
 
   useEffect(() => {
     const fetchPostsByLikes = async () => {
       try {
-        const response = await axios.get("/posts/");
-        const postsData = response.data.results;
-        // sort array of objects with Array.prototype.sort() method
-        const orderedPosts = postsData.sort((a, b) => b.likes_count - a.likes_count);
-        setPosts(orderedPosts);
+        const allPosts = [];
+        let nextPage = "/posts/";
+
+        // because of api pagination loop throught all pages is required
+        // each page's results are added to allPosts array 
+        while (nextPage) {
+          const response = await axios.get(nextPage);
+          const { results, next } = response.data;
+          allPosts.push(...results);
+          nextPage = next;
+        }
+
+        // sort the posts in descending order based on likes_count
+        const orderedPosts = allPosts.sort((a, b) => b.likes_count - a.likes_count);
+        // filter out posts with zero likes
+        const likedOrderedPosts = orderedPosts.filter((post) => post.likes_count >= 1);
+        setPosts(likedOrderedPosts.slice(0, 5)); // slice array to show first 5 results
+        setHasLoaded(true);
       } catch (error) {
-        console.error("Error fetching posts:", error);
+        console.error(error);
       }
     };
 
     fetchPostsByLikes();
-  }, []);
-
-  // filter posts to show only top5 posts from liked posts
-  const likedPosts = posts.filter((post) => post.likes_count > 0).slice(0, 5);
-  const hasLikedPosts = likedPosts.length > 0; // bool
+  }, [refreshLikes]);
 
   return (
     <>
-      {hasLikedPosts && (
-        <Container className={`${appStyles.Container}`}>
-          <h4 className="text-center pb-3">Trending posts</h4>
-          <Row>
-            <Col className="mx-auto">
-              {likedPosts.map((post) => (
-                <div key={post.id}>
-                  <Link to={`/posts/${post.id}`}>
-                    <span className={styles.TopHeart}>
-                      {post.likes_count} <i className={`far fa-heart`} />
-                    </span>
-                    <span>{post.title}</span>
-                  </Link>
-                </div>
-              ))}
-            </Col>
-          </Row>
+      {hasLoaded ? (
+        <>
+          {posts.length ? (
+            <Container className={`${appStyles.Container}`}>
+              <h4 className="text-center pb-3">Trending posts</h4>
+              <Row>
+                <Col className="mx-auto">
+                  {posts.map((post) => (
+                    <div key={post.id}>
+                      <Link to={`/posts/${post.id}`}>
+                        <span className={styles.TopHeart}>
+                          {post.likes_count} <i className={`far fa-heart`} />
+                        </span>
+                        <span>{post.title}</span>
+                      </Link>
+                    </div>
+                  ))}
+                </Col>
+              </Row>
+            </Container>
+          ) : null}
+        </>
+      ) : (
+        <Container className={`${appStyles.Container} text-center`}>
+          <Spinner />
         </Container>
       )}
     </>
   );
+  
 }
 
 export default TopPosts;
+
