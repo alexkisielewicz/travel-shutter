e16dfff614a4e300e669370e26fc02988e10ee6d
Aleksander Kisielewicz <aleksander.kisielewicz@gmail.com>
2023-06-24 17:02:21 +0000

    Add validation and input characters counter

--- src/pages/comments/CommentEditForm.js
+++ src/pages/comments/CommentEditForm.js
@@ -6,11 +6,15 @@ import { axiosRes } from "../../api/axiosDefaults";
 import btnStyles from "../../styles/Button.module.css"
 import styles from "../../styles/CommentCreateEditForm.module.css";
 import { toast } from 'react-toastify';
+import InputError from "../../components/InputError";
 
 function CommentEditForm(props) {
   const { id, content, setShowEditForm, setComments } = props;
-
   const [formContent, setFormContent] = useState(content);
+  const [errors, setErrors] = useState({});
+  const maxCharacterCount = 300;
+  const [characterCount, setCharacterCount] = useState("");
+  const [isLimit, setIsLimit] = useState(false);
 
   const showToast = (message) => {
     toast.success(message);
@@ -18,6 +22,20 @@ function CommentEditForm(props) {
 
   const handleChange = (event) => {
     setFormContent(event.target.value);
+  };
+
+  const handleInput = (event) => {
+    const input = event.target.value;
+    const remainingCharacters = maxCharacterCount - input.length;
+
+    if (remainingCharacters >= 0 && remainingCharacters <= maxCharacterCount) {
+      setFormContent(input.slice(0, maxCharacterCount));
+      setCharacterCount(`${remainingCharacters}/${maxCharacterCount}`);
+      setIsLimit(false);
+    } else {
+      setCharacterCount("Comment is too long!");
+      setIsLimit(true);
+    }
   };
 
   const handleSubmit = async (event) => {
@@ -40,8 +58,9 @@ function CommentEditForm(props) {
         }),
       }));
       setShowEditForm(false);
-    } catch (err) {
-      // console.log(err);
+    } catch (error) {
+      setErrors(error.response?.data);
+      // console.log(error.response?.data);
     }
   };
 
@@ -53,19 +72,26 @@ function CommentEditForm(props) {
           as="textarea"
           value={formContent}
           onChange={handleChange}
+          onInput={handleInput}
           rows={2}
         />
       </Form.Group>
-      <div className="text-right">
+      {errors.formContent && errors.content.map((message, idx) => (
+        <InputError key={idx} message={message} />
+      ))}
+      <div className="d-flex align-items-center">
+        <span className={`${styles.Counter} px-5`}>
+          {characterCount}
+        </span>
         <button
-          className={`${btnStyles.Button} ${btnStyles.Orange}`}
-          disabled={!content.trim()}
+          className={`${btnStyles.Button} ${btnStyles.Orange} btn ml-auto`}
+          disabled={!content.trim() || isLimit}
           type="submit"
         >
           save
         </button>
         <button
-          className={`${btnStyles.Button} ${btnStyles.OrangeOutline}`}
+          className={`${btnStyles.Button} ${btnStyles.OrangeOutline} btn`}
           onClick={() => setShowEditForm(false)}
           type="button"
         >
